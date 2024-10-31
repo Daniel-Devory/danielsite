cfb7f16c5b206b267e447d38d1e7883e94900bbf
Daniel-Devory <114166910+Daniel-Devory@users.noreply.github.com>
2024-10-30 22:09:38 -0400

    Made much needed changes to the Main page and the nav bar

--- JavaS/script.js
+++ JavaS/script.js
@@ -5,3 +5,7 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+function openPage(pageUrl){
+  window.location.href = pageUrl;
+}
